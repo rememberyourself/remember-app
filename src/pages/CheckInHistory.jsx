85b065062f5918083d7c9a9d18f29a3ac7bb89c8
Jack <jack@iMacPro.lan>
2026-02-15 20:34:05 +0100

    fix: new client-reply icon - larger, centered, transparent PNG + upload progress on replies

--- src/pages/CheckInHistory.jsx
+++ src/pages/CheckInHistory.jsx
@@ -198,7 +198,7 @@ function ClientReplyForm({ checkinId, onSubmitted, onCancel }) {
 // ===== Thread Message Display =====
 function ThreadMessage({ msg, isCoach }) {
   const borderColor = isCoach ? 'border-l-[#C9A96E]/60' : 'border-l-green-600/60';
-  const label = isCoach ? <><img src="/icons/coach-response.png" alt="" className="w-8 h-8 inline" /> Coach</> : <><img src="/icons/client-reply.svg" alt="" className="w-10 h-10 inline" /> You</>;
+  const label = isCoach ? <><img src="/icons/coach-response.png" alt="" className="w-8 h-8 inline" /> Coach</> : <><img src="/icons/client-reply.png" alt="" className="w-10 h-10 inline" /> You</>;
   const labelColor = isCoach ? 'text-gold-500' : 'text-green-400';
 
   return (
